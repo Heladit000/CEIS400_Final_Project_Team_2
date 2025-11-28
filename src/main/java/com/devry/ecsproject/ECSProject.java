b0991141cb9bf53fa6f081098b21474e89dfb988
Nicolás Prieto <91237535+Heladit000@users.noreply.github.com>
2025-11-28 16:38:15 -0500

    Started version of the project

---
+++ src/main/java/com/devry/ecsproject/ECSProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.devry.ecsproject;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ECSProject {
+
+    public static void main(String[] args) {
+        System.out.println("ECS system TEAM 2!");
+    }
+}
